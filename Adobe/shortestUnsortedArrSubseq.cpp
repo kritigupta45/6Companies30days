8c0156f58d302b8afb96a2e3b3373a94def820e4
Kriti <kriti45gupta@gmail.com>
2023-01-22 00:34:15 +0530

    day22: adobe it is!

---
+++ Adobe/shortestUnsortedArrSubseq.cpp
@@ -0,0 +1,16 @@
+// class Solution {
+// public:
+//     int findUnsortedSubarray(vector<int>& nums) {
+//         int ans = 0, i = 0, j = nums.size() - 1;
+//         int curr_max=INT_MAX, curr_min=INT_MIN, start=0,end=-1;        
+//         while(j >= 0){
+//             if(nums[i] >= curr_min) curr_min = nums[i];
+//             else end = i;
+//             if(nums[j] <= curr_max) curr_max = nums[j];
+//             else start = j;
+//             ++i;
+//             --j;
+//         }
+//         return end - start + 1;
+//     }
+// };
